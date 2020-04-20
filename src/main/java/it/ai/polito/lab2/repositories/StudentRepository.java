c0247561d1190be24c9da9121ecce0205b8086ab
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2020-04-20 16:35:34 +0200

    step 19. Missing one method implementation

--- src/main/java/it/ai/polito/lab2/repositories/StudentRepository.java
+++ src/main/java/it/ai/polito/lab2/repositories/StudentRepository.java
@@ -2,6 +2,8 @@ package it.ai.polito.lab2.repositories;
 
 import it.ai.polito.lab2.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
 }
