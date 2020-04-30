b985903a9539a751cbe8f3137b7034cc3946b1b1
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2020-04-30 16:45:36 +0200

    Inital commit

--- src/main/java/it/ai/polito/lab2/Lab2Application.java
+++ src/main/java/it/ai/polito/lab2/Lab2Application.java
@@ -23,45 +23,6 @@ import java.util.List;
 @SpringBootApplication
 public class Lab2Application {
 
-    //PER FARE I TEST HO UTILIZZATO Lab2ApplicationTests con JUNIT
-    //Se si vuole runnare i test lasciare commentato il metodo run()
-    @Bean
-    public CommandLineRunner runner(TeamService teamService){
-        return args -> {
-            //ADD AND ENROLL STUDENTS
-            /*
-            File file = new File("src/main/resources/static/students.csv");
-            try(Reader reader = new BufferedReader(new FileReader(file))){
-                System.out.println(teamService.addAndEnroll(reader, "Applicazioni Internet"));
-            }catch (Exception ex) {
-                System.out.println("An error occured while processing csv files: " + ex.getMessage());
-
-            }
-
-            System.out.println();
-
-            //GETTERS
-            System.out.println("GET SINGOLI:");
-            System.out.println(teamService.getCourse("Applicazioni Internet"));
-            System.out.println(teamService.getStudent("s1"));
-            System.out.println();
-            System.out.println("GET ALL");
-            System.out.println(teamService.getAllCourses());
-            System.out.println(teamService.getAllStudents());
-            System.out.println("GET ENROLLED STUDENTS");
-            System.out.println(teamService.getEnrolledStudents("Applicazioni Internet"));
-
-            //ADD COURSE
-            teamService.addCourse(new CourseDTO("OOP", 10, 300));
-
-            teamService.addStudentToCourse("s1", "Programmazione di Sistema");
-            teamService.addStudentToCourse("s1", "Reti");
-            System.out.println("GET STUDENT COURSES:\n" + teamService.getCourses("s1"));
-             */
-        };
-    }
-
-
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
